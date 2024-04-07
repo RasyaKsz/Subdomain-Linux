069c0a9ab4c335d7b0f3c58c33407f52d2fc1588
Rasya Fauzy <112188027+RasyaKsz@users.noreply.github.com>
2024-04-07 17:49:57 +0700

    active_vhost.sh

--- active_vhost.sh
+++ active_vhost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-for i in {1..50} 
-do 
+for i in {1..50}
+do
   if (($i < 10)); then 
     cp /etc/apache2/sites-available/rasya.conf /etc/apache2/sites-available/web0$i.conf
     sed -i /etc/apache2/sites-available/web0$i.conf -e "s/rasya/web0$i/g"
