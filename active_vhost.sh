76fd3e72ee95c3faa91c561410a6479413e78d92
Rasya Fauzy <112188027+RasyaKsz@users.noreply.github.com>
2024-04-07 17:54:12 +0700

    active_vhost.sh

--- active_vhost.sh
+++ active_vhost.sh
@@ -2,10 +2,8 @@
 for i in {1..50}
 do
   if (($i < 10)); then 
-    cp /etc/apache2/sites-available/rasya.conf /etc/apache2/sites-available/web0$i.conf
-    sed -i /etc/apache2/sites-available/web0$i.conf -e "s/rasya/web0$i/g"
+    a2ensite web0$i.conf
   else 
-    cp /etc/apache2/sites-available/rasya.conf /etc/apache2/sites-available/web$i.conf 
-    sed -i /etc/apache2/sites-available/web$i.conf -e "s/rasya/web$i/g" 
+    a2ensite web$i.conf
   fi 
 done
